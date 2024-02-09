9eeea9f61cce0ea2d16b3f587a698b0b7c56bcd3
uroot <uroot@test.com>
2024-02-09 12:26:16 +0200

    Adding files for lamp stack

---
+++ files/scripts/centos-lamp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+yum update -y --exclude=kernel
+
+yum install -y nano git unzip screen
+
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+# PHP
+yum install -y php php-cli php-common php-devel php-mysql
+
+# MySQL
+#
+yum install -y mysql mysql-server mysql-devel
+chkconfig --add mysqld
+chkconfig mysql start
+
+mysql -u root -e "SHOW DATABASES";
+
+# Download starter content
+#
+service httpd restart
